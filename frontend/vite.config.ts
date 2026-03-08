53d2c104a462e537ba3db8bda1f51c41656ad404
Natalya Polikashina <polikashina@gmail.com>
2026-03-08 23:49:54 +0100

    fix: add generateSW strategy to Vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
+      strategies: 'generateSW',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Exercise Planner',
@@ -22,12 +23,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'icons/icon-192.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512.png',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
